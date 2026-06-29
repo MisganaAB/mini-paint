9f936ab0b08844328392de4e0bad524ef238aa91
MisganaAB <misganatogod@gmail.com>
2026-06-29 14:19:29 +0300

    the basic functions identity_matrix multiply_matrix and multiply_point

---
+++ transformations/matrix.py
@@ -0,0 +1,37 @@
+import math
+from typing import List, Tuple
+
+Matrix3x3 = List[List[float]]
+Point2D   = Tuple[float, float] 
+
+def identity_matrix() -> Matrix3x3:
+    return [
+        [1.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0],
+    ]
+
+
+def multiply_matrix(matrix_a: Matrix3x3, matrix_b: Matrix3x3) -> Matrix3x3:
+    result: Matrix3x3 = [[0.0, 0.0, 0.0],
+                          [0.0, 0.0, 0.0],
+                          [0.0, 0.0, 0.0]]
+ 
+    for i in range(3):          # row of A
+        for j in range(3):      # column of B
+            total = 0.0
+            for k in range(3):  # shared dimension
+                total += matrix_a[i][k] * matrix_b[k][j]
+            result[i][j] = total
+ 
+    return result
+
+
+def multiply_point(matrix: Matrix3x3, point: Point2D) -> Point2D:
+    x, y = point
+
+    x_h = matrix[0][0] * x + matrix[0][1] * y + matrix[0][2] * 1.0
+    y_h = matrix[1][0] * x + matrix[1][1] * y + matrix[1][2] * 1.0
+    w_h = matrix[2][0] * x + matrix[2][1] * y + matrix[2][2] * 1.0
+
+    return (x_h / w_h, y_h / w_h)
